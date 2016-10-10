3786f42d6610e98c8ec9364b0fd54770fdc6a61f
Leandro Gonçalves <contato.leandrogoncalves@gmail.com>
2016-10-09 23:48:30 -0300

    fix listar turma v2

--- src/AcmeRocket_java/src/br/com/fiap/controller/CtrlListarTurma.java
+++ src/AcmeRocket_java/src/br/com/fiap/controller/CtrlListarTurma.java
@@ -2,12 +2,7 @@ package br.com.fiap.controller;
 
 
 import br.com.fiap.dao.PeriodoDAO;
-import br.com.fiap.dao.TurmaDAO;
 import br.com.fiap.entity.Periodo;
-import br.com.fiap.entity.Turma;
-import java.util.ArrayList;
-import java.util.List;
-
 import br.com.fiap.dao.TurmaDAO;
 import br.com.fiap.entity.Turma;
 import java.util.ArrayList;
@@ -18,17 +13,29 @@ public class CtrlListarTurma {
     public CtrlListarTurma() {
     }
     
+    /**
+     * Carrega lista de Periodos para preenchimento do combobox
+     * @return 
+     */
     public ArrayList<Periodo> CarregarRegistrosPeriodo(){
         PeriodoDAO dao = new PeriodoDAO();
         return dao.listar();
     }
     
+    /**
+     * Carrega a lista de turmas cadastradas
+     * @return 
+     */
     public ArrayList<Turma> CarregarRegistrosTurmas(){
         TurmaDAO dao = new TurmaDAO();
         return dao.listar();
     }
     
-
+    /**
+     * Retorna o nome do periodo atrelado a turma
+     * @param turma
+     * @return 
+     */
     public String obterNomePeriodo(Turma turma){
         PeriodoDAO dao = new PeriodoDAO();
         return dao.buscarNomePeriodo(turma.getCodPeriodo());
