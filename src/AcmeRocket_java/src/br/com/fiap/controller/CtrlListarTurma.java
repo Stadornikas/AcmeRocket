66405646b3321e1216a55f679ed1e1304c48199f
Leonardo <stadornikas@gmail.com>
2016-10-09 23:32:51 -0300

    Criando Ctrl ListarTurma

--- src/AcmeRocket_java/src/br/com/fiap/controller/CtrlListarTurma.java
+++ src/AcmeRocket_java/src/br/com/fiap/controller/CtrlListarTurma.java
@@ -1,5 +1,15 @@
 package br.com.fiap.controller;
 
+import br.com.fiap.dao.TurmaDAO;
+import br.com.fiap.entity.Turma;
+import java.util.ArrayList;
+
 public class CtrlListarTurma {
     
+    public CtrlListarTurma() {}
+    
+    public ArrayList<Turma> CarregarRegistros(){
+        TurmaDAO dao = new TurmaDAO();
+        return dao.listar();
+    }
 }
